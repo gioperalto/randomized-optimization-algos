19026c124dc22c7588f5d93114c1ccdb34a035cb
gioperalto <gyoperalto@gmail.com>
2020-10-08 19:12:07 -0400

    Added TSP problem

--- travelling_salesman.py
+++ travelling_salesman.py
@@ -1,12 +1,12 @@
 import numpy as np
 from util.plotter import Plotter
 from util.algos import randomized_hill_climb, simulated_annealing, genetic_algorithm, mimic
-from util.problems import queens_problem
+from util.problems import travelling_salesman_problem
 
 def plot_rhc(fitness_fn, name, label):
     plt = Plotter(
         name=name,
-        learner='optimization-problems/queens',
+        learner='optimization-problems/tsp',
         axes={ 'x': 'Iterations', 'y': 'Fitness(x)' }
     )
     plt.add_plot(
@@ -22,7 +22,7 @@ def plot_sa(problem, state, max_iters, name):
 
     plt = Plotter(
         name=name,
-        learner='optimization-problems/queens',
+        learner='optimization-problems/tsp',
         axes={ 'x': 'Iterations', 'y': 'Fitness(x)' },
         legend_title='Start temp'
     )
@@ -43,11 +43,11 @@ def plot_sa(problem, state, max_iters, name):
     plt.save()
 
 def plot_ga(problem, max_iters, name):
-    mutate_probs = [.1, .3, .5]
+    mutate_probs = [.3, .4, .5, .6]
 
     plt = Plotter(
         name=name,
-        learner='optimization-problems/queens',
+        learner='optimization-problems/tsp',
         axes={ 'x': 'Iterations', 'y': 'Fitness(x)' },
         legend_title='Mutation prob'
     )
@@ -57,6 +57,7 @@ def plot_ga(problem, max_iters, name):
             max_iters=max_iters,
             mutation_prob=mp
         )
+        print('GA (max):', max(ga_fitness))
         plt.add_plot(
             x=np.arange(1, len(ga_fitness)+1, dtype=int),
             y=ga_fitness, 
@@ -67,11 +68,11 @@ def plot_ga(problem, max_iters, name):
     plt.save()
 
 def plot_mimic(problem, max_iters, name):
-    keep_pcts = [.1, .3, .5, .7]
+    keep_pcts = [.1, .2, .3, .4]
 
     plt = Plotter(
         name=name,
-        learner='optimization-problems/queens',
+        learner='optimization-problems/tsp',
         axes={ 'x': 'Iterations', 'y': 'Fitness(x)' },
         legend_title='Keep (%)'
     )
@@ -81,6 +82,7 @@ def plot_mimic(problem, max_iters, name):
             max_iters=max_iters,
             keep_pct=kp
         )
+        print('MIMIC (max):', max(m_fitness))
         plt.add_plot(
             x=np.arange(1, len(m_fitness)+1, dtype=int),
             y=m_fitness, 
@@ -91,35 +93,45 @@ def plot_mimic(problem, max_iters, name):
     plt.save()
 
 if __name__ == "__main__":
-    init_state = np.arange(0, 10, 1, dtype=int)
-    problem, init_score = queens_problem(state=init_state)
+    seed = 93
+    np.random.seed(seed)
+    init_state = np.random.permutation((np.arange(0, 20, 1)))
+    problem, init_score = travelling_salesman_problem(state=init_state, seed=seed)
 
     rhc_state, rhc_score, rhc_fitness = randomized_hill_climb(
         problem=problem, 
         state=init_state, 
-        max_iters=100
+        max_iters=500
     )
     plot_rhc(
         fitness_fn=rhc_fitness, 
-        name='10-Queens - RHC',
+        name='Travelling Salesman - RHC',
         label='Random Hill Climb'
     )
+
+    print('RHC generated')
 
     plot_sa(
         problem=problem,
         state=init_state,
-        max_iters=100,
-        name='10-Queens - SA'
+        max_iters=500,
+        name='Travelling Salesman - SA'
     )
+
+    print('SA generated')
 
     plot_ga(
         problem=problem,
-        max_iters=100,
-        name='10-Queens - GA'
+        max_iters=20,
+        name='Travelling Salesman - GA'
     )
+
+    print('GA generated')
 
     plot_mimic(
         problem=problem,
-        max_iters=5,
-        name='10-Queens - MIMIC'
+        max_iters=20,
+        name='Travelling Salesman - MIMIC'
     )
+
+    print('MIMIC generated')
--- util/problems.py
+++ util/problems.py
@@ -1,14 +1,27 @@
 import six
 import sys
 sys.modules['sklearn.externals.six'] = six
-from mlrose import DiscreteOpt, Queens
+from mlrose import DiscreteOpt, TSPOpt, Queens, TravellingSales
 import numpy as np
 
 def queens_problem(state):
     fitness = Queens()
+
     return DiscreteOpt(
         length=state.shape[0], 
         fitness_fn=fitness, 
         maximize=False, 
         max_val=state.shape[0],
+    ), fitness.evaluate(state)
+
+def travelling_salesman_problem(state, seed):
+    coords, length = [], state.shape[0]
+    for i in range(length):
+        coords.append((np.random.randint(length), np.random.randint(state.shape[0])))
+    fitness = TravellingSales(coords=coords)
+
+    return TSPOpt(
+        length=length,
+        fitness_fn=fitness,
+        maximize=False
     ), fitness.evaluate(state)
